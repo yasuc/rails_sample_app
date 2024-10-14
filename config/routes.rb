fec7033c3bdca2d58d1f3d8d273345c310da20c9
IHA Yasushi <yasuc18@gmail.com>
2024-10-14 13:52:31 +0900

    feat: 🎸 Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root "application#hello"
+  root "static_pages#home"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
 end
